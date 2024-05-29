018a0f745574c63349805bbacb8da2a3763ad8f4
ILBSPT <andremanada@gmail.com>
2024-05-29 22:45:59 +0100

    query fixed for data warehouse

--- create_route_stops_table.sql
+++ create_route_stops_table.sql
@@ -1,23 +1,72 @@
 CREATE EXTENSION postgis;
 
+DROP TABLE route_stops;
+
 CREATE TABLE route_stops (
     route_id VARCHAR(10),
     stop_id VARCHAR(10),
     stop_lat numeric(10,8),
     stop_lon numeric(10,8),
     direction_id NUMERIC(2),
-    stop_sequence INTEGER
+    stop_sequence INTEGER,
+    last_departure TIME,
+    trip_headsign VARCHAR(50),
+    trip_headsign_hash INTEGER,
+    max_sequence INTEGER
+    --add last departure_time to minimize fails
 );
 
-INSERT INTO route_stops (route_id, stop_id, stop_lat, stop_lon, direction_id, stop_sequence)
+CREATE INDEX idx_max_sequence ON route_stops (max_sequence);
+CREATE INDEX idx_hash ON route_stops (trip_headsign_hash);
+CREATE INDEX idx_sequence ON route_stops (stop_sequence);
+-- Create a spatial index on the route_stops table
+CREATE INDEX route_stops_geom_idx ON route_stops USING GIST (ST_SetSRID(ST_Point(stop_lon, stop_lat), 4326));
+
+--INSERT INTO route_stops (route_id, stop_id, stop_lat, stop_lon, direction_id, stop_sequence)
+--SELECT 
+    --tr.route_id, 
+    --st.stop_id,
+    --s.stop_lat,
+    --s.stop_lon,
+    --tr.direction_id,
+    --st.stop_sequence
+
+--FROM 
+    --stop_times st
+--JOIN 
+    --stops s
+--ON 
+    --s.stop_id = st.stop_id
+--JOIN 
+    --trips tr
+--ON st.trip_id = tr.trip_id
+--WHERE tr.service_id = 'UTEIS'
+--GROUP BY 
+    --tr.route_id, 
+    --st.stop_id,
+    --s.stop_lon,
+    --s.stop_lat,
+    --tr.direction_id,
+    --st.stop_sequence
+--;
+
+INSERT INTO route_stops (route_id, stop_id, stop_lat, stop_lon, direction_id, stop_sequence, last_departure, trip_headsign, trip_headsign_hash, max_sequence)
+WITH max_st AS (
+    SELECT trip_id, MAX(stop_sequence) as max_seq
+    FROM stop_times
+    GROUP BY trip_id
+)
 SELECT 
     tr.route_id, 
     st.stop_id,
     s.stop_lat,
     s.stop_lon,
     tr.direction_id,
-    st.stop_sequence
-
+    st.stop_sequence,
+    MAX(st.departure_time) AS max_departure_time,
+    tr.trip_headsign,
+    hashtext(tr.trip_headsign),
+    max_st.max_seq
 FROM 
     stop_times st
 JOIN 
@@ -26,7 +75,11 @@ ON
     s.stop_id = st.stop_id
 JOIN 
     trips tr
-ON st.trip_id = tr.trip_id
+ON 
+    st.trip_id = tr.trip_id
+JOIN max_st 
+ON max_st.trip_id = st.trip_id
+    --AND tr.service_id = 'UTEIS'
 WHERE tr.service_id = 'UTEIS'
 GROUP BY 
     tr.route_id, 
@@ -34,5 +87,6 @@ GROUP BY
     s.stop_lon,
     s.stop_lat,
     tr.direction_id,
-    st.stop_sequence
-;
+    tr.trip_headsign,
+    max_st.max_seq,
+    st.stop_sequence;
--- fill_warehouse.py
+++ fill_warehouse.py
@@ -65,7 +65,8 @@ sql_get_trips_from_routes = '''
 SELECT
     route_id, 
     trip_id
-FROM trips t;
+FROM trips t
+WHERE service_id = 'UTEIS'
 '''
 cursor_psql.execute(sql_get_trips_from_routes)
 results = cursor_psql.fetchall()
@@ -81,8 +82,12 @@ for r in results:
 trip_stop_times = {}
 
 sql_get_stop_times = ''' 
-    SELECT trip_id, stop_id, departure_time 
-    FROM stop_times;
+    SELECT st.trip_id, stop_id, departure_time 
+    FROM stop_times st
+    JOIN trips t
+    ON st.trip_id = t.trip_id
+    WHERE t.service_id = 'UTEIS'
+    ;
 '''
 
 cursor_psql.execute(sql_get_stop_times)
@@ -91,34 +96,37 @@ results = cursor_psql.fetchall()
 for r in results:
     if trip_stop_times.get(r[0]) == None:
         trip_stop_times[r[0]] = {}
-    trip_stop_times[r[0]][r[1]] = r[2]
+    if trip_stop_times[r[0]].get(r[1]) == None:
+        trip_stop_times[r[0]][r[1]] = []
+    trip_stop_times[r[0]][r[1]].append(r[2])
 
 #-------------------------------------------------
 #-------------------------------------------------
 #-------------------------------------------------
-    
+
 main_query = ''' 
 SELECT
-    --t.id,
     s.route_id,
     s.init_stop_id,
     s.end_stop_id,
-    st_astext(st_transform(t.initial_point, 3763)) as initial_point,
-    st_astext(st_transform(t.final_point, 3763)) as final_point,
-    st_astext(st_transform(st_setsrid(st_point(s.init_lon, s.init_lat), 4326), 3763)) as stop_point_init,
-    st_astext(st_transform(st_setsrid(st_point(s.end_lon, s.end_lat), 4326), 3763)) as stop_point_end,
-    t.initial_ts,
-    t.final_ts
+    ST_AsText(ST_Transform(t.initial_point, 3763)) AS initial_point,
+    ST_AsText(ST_Transform(t.final_point, 3763)) AS final_point,
+    ST_AsText(ST_Transform(ST_SetSRID(ST_Point(s.init_lon, s.init_lat), 4326), 3763)) AS stop_point_init,
+    ST_AsText(ST_Transform(ST_SetSRID(ST_Point(s.end_lon, s.end_lat), 4326), 3763)) AS stop_point_end,
+    CAST(TO_TIMESTAMP(t.initial_ts) AS TIME),
+    CAST(TO_TIMESTAMP(t.final_ts) AS TIME)
 FROM taxi_services t
 JOIN LATERAL (
     WITH p1 AS (
         SELECT 
-            stop_id
+            stop_id,
+            stop_lat,
+            stop_lon
         FROM 
-            stops s
+            stops
         ORDER BY 
             ST_Distance(
-                ST_Transform(ST_SetSRID(ST_Point(s.stop_lon, s.stop_lat), 4326), 3763), 
+                ST_Transform(ST_SetSRID(ST_Point(stop_lon, stop_lat), 4326), 3763), 
                 ST_Transform(t.initial_point, 3763)
             )
         LIMIT 5
@@ -128,6 +136,8 @@ JOIN LATERAL (
             rs.route_id,
             rs.direction_id,
             rs.stop_sequence,
+            rs.max_sequence,
+            rs.trip_headsign_hash,
             p1.stop_id,
             rs.stop_lon,
             rs.stop_lat
@@ -135,51 +145,47 @@ JOIN LATERAL (
             route_stops rs
         JOIN p1 
         ON rs.stop_id = p1.stop_id
-        GROUP BY
-            rs.route_id,
-            rs.direction_id,
-            rs.stop_sequence,
-            p1.stop_id,
-            rs.stop_lon,
-            rs.stop_lat
+        WHERE CAST(TO_TIMESTAMP(t.initial_ts) AS TIME) <= rs.last_departure
+    ),
+    s AS (
+        SELECT
+            p2.stop_id AS init_stop_id,
+            p2.stop_lat AS init_lat,
+            p2.stop_lon AS init_lon,
+            rs.stop_id AS end_stop_id,
+            rs.stop_lat AS end_lat,
+            rs.stop_lon AS end_lon,
+            rs.route_id
+        FROM
+            route_stops rs
+        JOIN
+            p2
+        ON rs.route_id = p2.route_id 
+        WHERE
+            p2.direction_id = rs.direction_id
+            AND p2.trip_headsign_hash = rs.trip_headsign_hash
+            AND p2.stop_sequence < rs.stop_sequence
+            AND p2.max_sequence = rs.max_sequence
+            AND CAST(TO_TIMESTAMP(t.final_ts) AS TIME) <= rs.last_departure
+        ORDER BY 
+            ST_Distance(
+                ST_Transform(ST_SetSRID(ST_Point(rs.stop_lon, rs.stop_lat), 4326), 3763),
+                ST_Transform(t.final_point, 3763)
+            )
+        LIMIT 1
     )
     SELECT
-        p2.stop_id as init_stop_id,
-        p2.stop_lat as init_lat,
-        p2.stop_lon as init_lon,
-        rs.stop_id as end_stop_id,
-        rs.stop_lat as end_lat,
-        rs.stop_lon as end_lon,
-        rs.route_id
-    FROM
-        route_stops rs
-    JOIN
-        p2
-    ON rs.route_id = p2.route_id
-    WHERE
-        p2.direction_id = rs.direction_id
-        AND p2.stop_sequence < rs.stop_sequence
-    GROUP BY 
-        p2.stop_id,
-        p2.stop_lat,
-        p2.stop_lon,
-        rs.stop_id,
-        rs.stop_lat,
-        rs.stop_lon,
-        rs.route_id
-    ORDER BY 
-        ST_Distance(
-            ST_ClosestPoint(
-                ST_Collect(ST_Transform(ST_SetSRID(ST_Point(rs.stop_lon, rs.stop_lat), 4326), 3763)),
-                ST_Transform(t.final_point, 3763)
-            ), 
-            ST_Transform(t.final_point, 3763)
-        )
-        --ordenar pela soma das distancias
-        
-    LIMIT 1
+        s.route_id,
+        s.init_stop_id,
+        s.end_stop_id,
+        s.init_lat,
+        s.init_lon,
+        s.end_lat,
+        s.end_lon
+    FROM s
+
 ) AS s ON TRUE
-LIMIT 100
+LIMIT 100000;
 '''
 
 results = []
@@ -202,35 +208,50 @@ for r in results:
 
 #get stop times
 
+failed_data_points = 0
+
 for t in trips:
     trip = ''
-    best_init_time = -1
     best_time_found = False
+    best_init = -1
+    best_end = -1
 
-    t_init = datetime.fromtimestamp(t.taxi_init_time).time()
-
+    #print(t.taxi_init_time)
+    t_init = t.taxi_init_time
     for tr in trips_in_routes[t.route_id]:
         if(trip_stop_times.get(tr) == None): continue
         if(trip_stop_times[tr].get(t.bus_init_stop) == None): continue
         if(trip_stop_times[tr].get(t.bus_end_stop) == None): continue
 
-        if(trip_stop_times[tr][t.bus_init_stop] > t_init):
-            if(best_time_found):
-                if trip_stop_times[tr][t.bus_init_stop] < best_init_time:
-                    best_init_time = trip_stop_times[tr][t.bus_init_stop]
-                    trip = tr
-            else:
-                best_init_time = trip_stop_times[tr][t.bus_init_stop]
-                best_time_found = 1
-                trip = tr
-    t_end = -1
+
+        for init in trip_stop_times[tr][t.bus_init_stop]:
+            for end in trip_stop_times[tr][t.bus_end_stop]:
+                #print(init, end)
+                if(init > t_init):
+                    if(init < end):
+                        if(best_time_found):
+                            if init < best_init and end < best_end:
+                            #if end < best_end:
+                                best_init = init
+                                best_end = end
+                                trip = tr
+                        else:
+                            best_init = init
+                            best_end = end
+                            best_time_found = 1
+                            trip = tr
+
 
     if(trip != ''):
-        #print("trip", trip, "end stop", t.bus_end_stop)
-        t_end = trip_stop_times[trip][t.bus_end_stop]
-        t.set_times(trip, best_init_time, t_end)
+        if(best_end < best_init):
+            #print("error", trip, best_init, best_end)
+            #print("trip time", t_init, "begin stop:", t.bus_init_stop, "end stop:", t.bus_end_stop, "route", t.route_id)
+            failed_data_points += 1
+            continue
+        t.set_times(trip, best_init, best_end)
     else:
-        print("trip time", t_init, "begin stop:", t.bus_init_stop, "end stop:", t.bus_end_stop, "route", t.route_id)
+        #print("trip time", t_init, "begin stop:", t.bus_init_stop, "end stop:", t.bus_end_stop, "route", t.route_id, "trip", t.trip_id)
+        failed_data_points += 1
 
 trips = [t for t in trips if t.trip_id != -1]
 
@@ -244,3 +265,7 @@ with open("fill_warehouse.sql", "w+") as f:
     for t in trips:
         print(t, file=f)
 
+#for t in trips:
+    #print(t)
+
+print("failed entries", failed_data_points)
